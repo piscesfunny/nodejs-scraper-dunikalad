1d55bac8241a079fc2fab9b8b4ddca577d66a1f3
fullstack <you@example.com>
2020-01-24 00:09:46 +0100

    Added CitizensInformation.js

---
+++ sources/CitizensInformation.js
@@ -0,0 +1,59 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+let listingUrl = 'https://www.citizensinformationboard.ie/en/news/vacancies.html';
+
+
+const getListings = async (address, searchTerm, pageUrl=listingUrl) => {
+    let listings = [];
+
+    const response = await request({
+        resolveWithFullResponse: true,
+        uri: pageUrl,
+    });
+    const $ = cheerio.load(response.body);
+    const urlElements = $('ul.newslist li');
+
+    let urlPortion, fullUrl, title, temp;
+    urlElements.map((key, item) => {
+        title = $(item).find('a').text().trim()
+        urlPortion = $(item).find('a').attr('href');
+        if (urlPortion) {
+            urlPortion = urlPortion.trim();
+            if (urlPortion.indexOf('http') === -1)
+                fullUrl = `https://www.citizensinformationboard.ie${urlPortion}`;
+            else
+                fullUrl = urlPortion
+
+            listings.push({
+                'url': fullUrl,
+                'title': title
+            })
+        }
+
+    });
+
+    return listings;
+};
+
+const scrapePage = async ({ url, $, existingData }) => {
+    let description = $('div.content').html().trim();
+
+    const company = 'CitizensInformation';
+    const formattedAddress = 'Ireland  ';
+
+    const data = {
+        url,
+        description,
+        formattedAddress,
+        company,
+        ...existingData
+    };
+    return data;
+};
+
+module.exports = {
+    scrapePage,
+    getListings,
+    listingUrl,
+};
